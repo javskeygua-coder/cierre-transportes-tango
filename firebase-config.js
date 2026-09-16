5dff456702a669aa1933719f877e4cc08912c3ed
javskeygua-coder <javskeygua@gmail.com>
2026-09-16 00:26:51 -0700

    Versión completa: sincronización, consolidado y vouchers

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.TANGO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAx3Aik8HNJp5C01x4FHzoICnZBS133Ufs",
+  authDomain: "cierre-transportes-tango.firebaseapp.com",
+  projectId: "cierre-transportes-tango",
+  storageBucket: "cierre-transportes-tango.firebasestorage.app",
+  messagingSenderId: "307760492600",
+  appId: "1:307760492600:web:c588bd716a3e26913fb6cd",
+  teamId: "5fe08496a1e37f0cbc60580cf780d86a"
+};
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='cierre-tango-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='cierre-tango-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./firebase-config.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
